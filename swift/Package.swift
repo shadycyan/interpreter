155fb87bb7ae41121d32c98ebc177c6a26c123ca
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-04-11 22:43:25 +0330

    style: convert indentation from spaces to tabs

--- swift/Package.swift
+++ swift/Package.swift
@@ -4,11 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Monkey",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "Monkey"),
-    ]
+	name: "Monkey",
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.executableTarget(
+			name: "Monkey"
+		),
+	]
 )
