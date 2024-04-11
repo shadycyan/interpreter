d8ac20b9e0d104ce55bf00cb60860ba1cc80be63
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-04-11 17:44:05 +0330

    feat: define Token for lexer

---
+++ swift/Sources/Token/Token.swift
@@ -0,0 +1,44 @@
+//
+//  Token.swift
+//  Monkey
+//
+//  Created by Cyan on 4/11/24.
+//
+
+import Foundation
+
+enum Token {
+	case illegal(String)
+	case eof
+	case ident(String)
+	case int(String)
+	case assign
+	case plus
+	case comma
+	case semi
+	case lParen
+	case rParen
+	case lSquirly
+	case rSquirly
+	case function
+	case `let`
+
+	var literal: String {
+		switch self {
+		case let .illegal(literal): return literal
+		case .eof: return "EOF"
+		case let .ident(literal): return literal
+		case let .int(literal): return literal
+		case .assign: return "="
+		case .plus: return "+"
+		case .comma: return ","
+		case .semi: return ";"
+		case .lParen: return "("
+		case .rParen: return ")"
+		case .lSquirly: return "{"
+		case .rSquirly: return "}"
+		case .function: return "fn"
+		case .let: return "let"
+		}
+	}
+}
