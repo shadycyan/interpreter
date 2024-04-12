6408ce7af82e218e280c8eb94498bc8f03e264cb
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-04-12 13:38:32 +0330

    test: extend the test case in LexerTests

--- swift/Tests/LexerTests.swift
+++ swift/Tests/LexerTests.swift
@@ -4,17 +4,53 @@ import XCTest
 
 final class LexerTests: XCTestCase {
 	func testNextToken() {
-		let input = "=+(){},;"
+		let input = """
+		input := `let five = 5;
+		let ten = 10;
+
+		let add = fn(x, y) {
+			x + y;
+		};
+
+		let result = add(five, ten);
+		"""
 
 		let tokens: [Token] = [
+			.let,
+			.ident("five"),
 			.equal,
-			.plus,
+			.int("5"),
+			.semi,
+			.let,
+			.ident("ten"),
+			.equal,
+			.int("10"),
+			.semi,
+			.let,
+			.ident("add"),
+			.equal,
+			.function,
 			.lParen,
+			.ident("x"),
+			.comma,
+			.ident("y"),
 			.rParen,
 			.lSquirly,
-			.rSquirly,
-			.comma,
+			.ident("x"),
+			.plus,
+			.ident("y"),
 			.semi,
+			.let,
+			.ident("result"),
+			.equal,
+			.ident("add"),
+			.lParen,
+			.ident("five"),
+			.comma,
+			.ident("ten"),
+			.rParen,
+			.semi,
+			.eof,
 		]
 
 		var lexer = Lexer(input: input)
