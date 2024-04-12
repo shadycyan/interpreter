5fb7d9e778741abf580461db50ec4d0c83de7baa
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-04-12 03:46:32 +0330

    feat: implement getNextToken and readChar in Lexer

--- swift/Sources/Lexer/Lexer.swift
+++ swift/Sources/Lexer/Lexer.swift
@@ -10,15 +10,49 @@ import Token
 
 public struct Lexer {
 	private let input: String
-	private var position = 0 // current position in input (points to current char)
-	private var readPosition = 0 // current reading position in input (after current char)
-	private var character: UInt8 = .zero // current char under examination
+	private var position: String.Index // current position in input (points to current char)
+	private var readPosition: String.Index // current reading position in input (after current char)
+	private var character: Character = "\0" // current char under examination
 
 	init(input: String) {
 		self.input = input
+		position = input.startIndex
+		readPosition = input.startIndex
+		readChar()
 	}
 
-	func getNextToken() -> Token {
-		.eof
+	mutating func getNextToken() throws -> Token {
+		var token: Token
+
+		switch character {
+		case "{": token = .lSquirly
+		case "}": token = .rSquirly
+		case "(": token = .lParen
+		case ")": token = .rParen
+		case ",": token = .comma
+		case ";": token = .semi
+		case "+": token = .plus
+		case "=": token = .equal
+		case "\0": token = .eof
+		default: throw LexerError.unexpectedCharacter(Character(UnicodeScalarType(character)))
+		}
+
+		readChar()
+		return token
 	}
+
+	mutating func readChar() {
+		if readPosition >= input.endIndex {
+			character = "\0"
+		} else {
+			character = input[readPosition]
+		}
+
+		position = readPosition
+		readPosition = input.index(after: readPosition)
+	}
+}
+
+enum LexerError: Error {
+	case unexpectedCharacter(Character)
 }
