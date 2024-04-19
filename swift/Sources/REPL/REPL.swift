72809638081fab911d4a9da60d20ca0a22eb9af1
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-04-19 16:29:16 +0330

    refactor: remove unused REPL enum

--- swift/Sources/REPL/REPL.swift
+++ swift/Sources/REPL/REPL.swift
@@ -8,22 +8,20 @@
 import Lexer
 import Token
 
-public enum REPL {
-	public static func start() {
-		let prompt = ">> ", greenColor = "\u{001B}[0;32m"
-		print(greenColor + prompt, terminator: "")
-		guard let lines = readLine() else { return }
+public func start() {
+	let prompt = ">> ", greenColor = "\u{001B}[0;32m"
+	print(greenColor + prompt, terminator: "")
+	guard let lines = readLine() else { return }
 
-		for line in lines.split(separator: "\n") {
-			var tokenizer = Lexer(input: String(line))
+	for line in lines.split(separator: "\n") {
+		var tokenizer = Lexer(input: String(line))
 
-			var token: Token
-			repeat {
-				token = tokenizer.nextToken()
-				print(token)
-			} while token != .eof
-		}
-
-		start()
+		var token: Token
+		repeat {
+			token = tokenizer.nextToken()
+			print(token)
+		} while token != .eof
 	}
+
+	start()
 }
